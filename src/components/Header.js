b8131df775ffafb431d2c736dcc38882deaf1727
Margarita <m.callagova@gmail.com>
2024-02-12 21:06:44 +0900

    add top bg component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,21 @@
 // TODO: Add REACT component with picture on top of page
+import React from 'react';
+import headerBG from "../assets/ref.png"
+import "./Header.css"
+
+class Header extends React.Component {
+    render() {
+       return(
+           <div className="headerBGContainer">
+               <img
+                   className="headerImage"
+                   src={headerBG}
+                   alt="Header Image"
+               />
+           </div>
+       )
+    }
+
+}
+
+export default Header;
