53629059789357de282c05249a789252901c1158
Margarita <m.callagova@gmail.com>
2024-03-06 22:26:51 +0900

    Add icon Add title Add classes for style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,33 @@
 // TODO: Add REACT component with picture on top of page
 import React from 'react';
 import headerBG from "../assets/ref.png"
+import iconImg from "../assets/headerIcon.jpg"
 import "./Header.css"
 
 class Header extends React.Component {
     render() {
-       return(
-           <div className="headerBGContainer">
-               <img
-                   className="headerImage"
-                   src={headerBG}
-                   alt="Header Image"
-               />
-               {/* TODO: add icon */}
-               {/*TODO: add header of page */}
-           </div>
-       )
+        return (
+            <div className="top-header-container">
+                <div className="header-bg-container">
+                    <img
+                        className="header-image"
+                        src={headerBG}
+                        alt="Header Image"
+                    />
+                </div>
+                <div className="icon-img-container">
+                    <img
+                        className="icon-image"
+                        src={iconImg}
+                        alt="Icon Image"
+                    />
+                </div>
+                <div className="app-title">
+                    <h1>Study</h1>
+                </div>
+            </div>
+
+        )
     }
 
 }
